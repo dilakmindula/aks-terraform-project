bbcaeefa78d7c95a2bbbb4d47b546f084970f668
dilakmindula <dilakmindula@gmail.com>
2025-03-10 11:42:14 +0530

    Initial commit

---
+++ main.tf
@@ -0,0 +1,26 @@
+provider "azurerm" {
+  subscription_id = "c7898be1-3990-443b-bb9d-4141c26ffedd"
+  features {}
+}
+
+resource "azurerm_resource_group" "aks_rg" {
+  name     = "aks-resource-group"
+  location = "East US"
+}
+
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = "myAKSCluster"
+  location           = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  dns_prefix          = "myaksdns"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
